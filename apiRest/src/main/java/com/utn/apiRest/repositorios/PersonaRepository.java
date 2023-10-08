f81ab07f002e5daf98076e7fac4550722c5b8819
sofiDiFrancisco <142851934+sofiDiFrancisco@users.noreply.github.com>
2023-10-08 18:30:36 -0300

    Delete apiRest directory

--- apiRest/src/main/java/com/utn/apiRest/repositorios/PersonaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.utn.apiRest.repositorios;
-
-import com.utn.apiRest.entidades.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonaRepository extends BaseRepository <Persona, Long> {
-}
--- apiRest/src/main/java/com/utn/apiRest/servicios/PersonaService.java
+++
@@ -1,6 +0,0 @@
-package com.utn.apiRest.servicios;
-
-import com.utn.apiRest.entidades.Persona;
-
-public interface PersonaService extends BaseService <Persona, Long>{
-}
